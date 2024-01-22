bba50a5b611a72554200813ffbaf2af53ddb3024
Divrun <denis.03.ermakov@gmail.com>
2024-01-22 15:19:09 +0500

    Fixed a bug with non-existent categories on DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
-        Category::factory(5)->create();
         Product::factory(250)->create();
 
         $this->orders();
