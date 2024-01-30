068986b0f3ec186bf8f08019ca8d7963fd9e47fa
Divrun <denis.03.ermakov@gmail.com>
2024-01-31 01:55:20 +0500

    Refactoring: uikit and css moved to assets folder. Unnecessary middleware removed.

--- resources/js/admin/app.js
+++ resources/js/admin/app.js
@@ -1,14 +1,11 @@
-import '../uikit';
-import '../uikit-icons'
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+
+import '../../assets/scripts/uikit';
+import '../../assets/scripts/uikit-icons'
+
 import App from './App.vue';
 import router from './router';
-import axios from 'axios';
-
-window.axios = axios;
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api'
-axios.defaults.withCredentials = true;
 
 const pinia = createPinia()
 const app = createApp(App)
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,7 +13,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Sora:wght@100;300;400;600;800&display=swap" rel="stylesheet">
 
-    @vite('resources/css/app.css')
+    @vite('resources/assets/css/app.css')
 </head>
 <body>
     <div id="app"></div>
