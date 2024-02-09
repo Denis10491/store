aff1679b1bb3a3169be19d2aed06e59e94667e0d
Divrun <denis.03.ermakov@gmail.com>
2024-02-09 18:58:07 +0500

    App.vue has been rewritten to Composition API. The other directory has been renamed to layout.

--- resources/js/user/App.vue
+++ resources/js/user/App.vue
@@ -1,22 +1,14 @@
+<script setup lang="ts">
+import Header from './components/layout/Header.vue'
+import Message from './components/layout/Message.vue'
+</script>
+
 <template>
     <main class="main container">
         <Header></Header>
-
         <div class="content">
             <router-view></router-view>
         </div>
     </main>
-    
-    <Message></Message>
+    <Message />
 </template>
-
-<script>
-import Header from './components/other/Header.vue'
-import Message from './components/other/Message.vue'
-
-export default {
-    components: {
-        Header, Message
-    }
-}
-</script>
---
+++ resources/js/user/components/layout/Header.vue
@@ -0,0 +1,60 @@
+<template>
+    <header class="uk-card uk-card-default uk-flex uk-flex-between uk-flex-middle uk-flex-center uk-background-default uk-width-1-1 uk-padding-small border">
+        <ul v-if="isAuth" class="uk-nav uk-nav-default uk-width-1-1 uk-flex uk-flex-middle">
+            <a href="/" class="header__logo">Store</a>
+            <li><router-link to="/basket" class="header__nav-item">Basket</router-link></li>
+            <li><router-link to="/about" class="header__nav-item">About</router-link></li>
+            <li><router-link to="/profile" class="header__nav-item">Profile</router-link></li>
+        </ul>
+        <ul v-else class="uk-nav uk-nav-default uk-width-1-1 uk-flex uk-flex-middle">
+            <a href="/" class="header__logo">Store</a>
+            <li><router-link to="/about" class="header__nav-item">About</router-link></li>
+            <li><router-link to="/basket" class="header__nav-item">Basket</router-link></li>
+        </ul>
+
+        <a href="https://github.com/Divrun/Store-Laravel-Vue" target="_blanc" class="uk-icon-button" uk-icon="github"></a>
+        <a v-if="isAuth" href="#" class="uk-icon-button" uk-icon="sign-out" @click="exit()"></a>
+        <nav v-else class="uk-flex uk-flex-middle">
+            <router-link to="/login" class="header__nav-item">Login</router-link>
+            <router-link to="/signup" class="header__nav-item uk-button uk-button-primary border">Singup</router-link>
+        </nav>
+    </header>
+</template>
+
+<script>
+import { logout } from '../../services/api';
+import { useUserStore } from '../../store/user';
+
+export default {
+    name: 'HeaderComponent',
+    setup() {
+        const userStore = useUserStore();
+        return { userStore }
+    },
+
+    methods: {
+        async exit() {
+            const isLogoutStatus = await logout();
+            if (isLogoutStatus) window.location.href = '/';
+        }
+    },
+
+    computed: {
+        isAuth() {
+            return this.userStore.isAuthStatus ?? false;
+        }
+    }
+}
+</script>
+
+<style scoped>
+header {
+    margin: 0 0 20px;
+}
+header a {
+    text-transform: uppercase;
+    letter-spacing: 1.2px;
+    font-size: 13px;
+    margin: 0 0 0 15px;
+}
+</style>
