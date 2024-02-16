90646edb151ba0410ec1832de68963ca4142e02b
Divrun <denis.03.ermakov@gmail.com>
2024-02-16 10:39:09 +0500

    Migration renamed orders_products to order_products.

--- database/migrations/2024_01_17_062520_order_products.php
+++ database/migrations/2024_01_17_062520_order_products.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('orders_products', function (Blueprint $table) {
+        Schema::create('order_products', function (Blueprint $table) {
             $table->id();
             $table->foreignId("product_id")->constrained("products")->onDelete("cascade");
             $table->foreignId("order_id")->constrained("orders")->onDelete("cascade");
