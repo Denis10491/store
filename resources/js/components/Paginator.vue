657267c8a88f4678700660a18eb2e7f60b5d32c4
Divrun <denis.03.ermakov@gmail.com>
2024-02-17 08:41:12 +0500

    Paginator component rewritten to Composition API and TS.

--- resources/js/components/Paginator.vue
+++ resources/js/components/Paginator.vue
@@ -1,7 +1,6 @@
 <template>
 <ul class="uk-pagination uk-flex-center" uk-margin>
     <li @click="handleChangePage(currentPage - 1)"><a href="#"><span uk-pagination-previous></span></a></li>
-
     <li
         :class="{
             'uk-active': currentPage == 1
@@ -10,7 +9,6 @@
     >
         <a href="#">1</a>
     </li>
-
     <li class="uk-disabled" v-if="range[0] > 2"><span>…</span></li>
     <li v-for="page in range" 
         :key="page"
@@ -21,8 +19,7 @@
     >
         <a href="#">{{ page }}</a>
     </li>
-    <li class="uk-disabled" v-if="range[range.length - 1] < this.numOfMaxPage - 1"><span>...</span></li>
-
+    <li class="uk-disabled" v-if="range[range.length - 1] < numOfMaxPage - 1"><span>...</span></li>
     <li
         :class="{
             'uk-active': currentPage == numOfMaxPage
@@ -31,56 +28,48 @@
     >
         <a href="#">{{ numOfMaxPage }}</a>
     </li>
-
     <li @click="handleChangePage(currentPage + 1)"><a href="#"><span uk-pagination-next></span></a></li>
 </ul>
 </template>
 
-<script>
-export default {
-    name: 'PaginatorComponent',
-    props: [ 'currentPage', 'changePage', 'numOfMaxPage' ],
+<script setup lang="ts">
+import { computed, ref } from 'vue';
 
-    data() {
-        return {
-            maxInRange: 5
+const props = defineProps<{
+    currentPage: number,
+    changePage: number,
+    numOfMaxPage: number
+}>()
+const emit = defineEmits<{
+  (e: 'changePage', num: number): Promise<void>,
+}>()
+
+const maxInRange = ref<number>(5)
+
+const handleChangePage = (num: number) => emit('changePage', num);
+
+const range = computed<Array<number>>(() => {
+    let range = [];
+    if (props.currentPage === 1) {
+        for(let i = 2; i < maxInRange.value; i++) {
+            range.push(i);
         }
-    },
-
-    computed: {
-        range() {
-            let range = [];
-            if (this.currentPage == 1) {
-                for(let i = 2; i < this.maxInRange; i++) {
-                    range.push(i);
-                }
-                return range;
-            }
-
-            if (this.currentPage == this.numOfMaxPage) {
-                for(let i = this.numOfMaxPage + 1 - this.maxInRange; i < this.numOfMaxPage - 1; i++) {
-                    range.push(i);
-                }
-                return range;
-            }
-
-            for(let i = this.currentPage - 2; i < this.currentPage; i++) {
-                if (i <= 1 || i == this.numOfMaxPage) continue;
-                range.push(i)
-            }
-            for(let i = this.currentPage; i < this.currentPage + 3; i++) {
-                if (i >= this.numOfMaxPage) break;
-                range.push(i)
-            }
-
-            return range;
-        }
-    },
-
-    methods: {
-        handleChangePage(num) {
-            this.changePage(num);
-        }
+        return range;
     }
-}
+    if (props.currentPage === props.numOfMaxPage) {
+        for(let i = props.numOfMaxPage + 1 - maxInRange.value; i < props.numOfMaxPage - 1; i++) {
+            range.push(i);
+        }
+        return range;
+    }
+    for(let i = props.currentPage - 2; i < props.currentPage; i++) {
+        if (i <= 1 || i == props.numOfMaxPage) continue;
+        range.push(i)
+    }
+    for(let i = props.currentPage; i < props.currentPage + 3; i++) {
+        if (i >= props.numOfMaxPage) break;
+        range.push(i)
+    }
+    return range;
+});
 </script>
