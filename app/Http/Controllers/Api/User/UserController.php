d3f7f9d4c4cd1092f9ad8285c0cfac6ab61e906e
Divrun <denis.03.ermakov@gmail.com>
2024-03-06 15:00:53 +0500

    fix(user): created logs for user requests.

--- app/Http/Controllers/Api/User/UserController.php
+++ app/Http/Controllers/Api/User/UserController.php
@@ -6,11 +6,13 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\User\UserResource;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
     public function show(): JsonResponse
     {
+        Log::info(Auth::user()->email.' received his data');
         return response()->json(new UserResource(Auth::user()));
     }
 }
