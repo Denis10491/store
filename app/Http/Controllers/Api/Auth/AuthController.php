5294ee661d5b5fb623b76f6c235fe5ac24231d17
Divrun <denis.03.ermakov@gmail.com>
2024-03-06 15:01:33 +0500

    feat(auth): created logs for auth requests.

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -8,23 +8,28 @@ use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Http\Resources\User\UserResource;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
     public function register(RegisterRequest $request, AuthServiceContract $service): JsonResponse
     {
         $createdUser = $service->store($request);
+        Log::info($createdUser->email.' created an account');
         return response()->json(new UserResource($createdUser), 201);
     }
 
     public function login(LoginRequest $request, AuthServiceContract $service): JsonResponse
     {
         $token = $service->login($request);
+        Log::info($request->str('email').' is logged in');
         return response()->json(['token' => $token]);
     }
 
     public function logout(): JsonResponse
     {
+        Log::info(Auth::user()->email.' has logged out');
         auth()->user()->tokens()->delete();
         return responseOk();
     }
