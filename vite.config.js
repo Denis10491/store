3efcffd36a3e8344503b809591b6abc35a901bc9
Divrun <denis.03.ermakov@gmail.com>
2024-04-02 09:38:12 +0500

    feat(configuration): frontend configuration updated. aliases added.

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,27 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import {defineConfig} from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+import path from "node:path";
 
 export default defineConfig({
     plugins: [
         vue(),
         laravel({
-            input: ['resources/assets/css/app.css', 'resources/js/user/app.js', 'resources/js/admin/app.js'],
+            input: [
+                "resources/assets/css/app.css",
+                "resources/js/user/app.ts",
+                "resources/js/admin/app.ts",
+            ],
             refresh: true,
         }),
     ],
-    resolve: { 
+    resolve: {
         alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
-            '@': path.resolve(__dirname, './resources'),
-            "@assets": path.resolve(__dirname, './resources/assets'),
-            "@middleware": path.resolve(__dirname, 'resources/js/user/middleware'),
-            '@components': path.resolve(__dirname, 'resources/js/components'),
-            '@user/components': path.resolve(__dirname, 'resources/js/user/components'),
-            "@user/services": path.resolve(__dirname, 'resources/js/user/services'),
-            "@user/store": path.resolve(__dirname, './resources/js/user/store'),
-            '@admin/components': path.resolve(__dirname, 'resources/js/admin/components'),
-            "@admin/services": path.resolve(__dirname, 'resources/js/admin/services'),
-            "@admin/store": path.resolve(__dirname, './resources/js/admin/store'),
-            '@helpers': path.resolve(__dirname, './resources/js/helpers')
+            "@assets": path.resolve(__dirname, "./resources/assets"),
+            "@helpers": path.resolve(__dirname, "./resources/js/helpers"),
+            "@ui": path.resolve(__dirname, "./resources/js/ui"),
+            "@user": path.resolve(__dirname, "./resources/js/user"),
+            "@admin": path.resolve(__dirname, "./resources/js/admin"),
         },
-    }
+    },
 });
